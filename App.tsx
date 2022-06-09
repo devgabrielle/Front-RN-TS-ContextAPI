29da5f769d6bb61d0b9b1d8cec06ac7a5f48b321
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2022-06-09 17:14:03 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {AuthProvider} from "./src/contexts/auth";
+
+import Routes from './src/routes/index';
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </NavigationContainer>
+  );
+};
+export default App;
